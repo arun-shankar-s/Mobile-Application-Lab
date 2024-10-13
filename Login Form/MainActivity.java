dbc8614d42cff609d6f2894b7fb6bd3d10af57a2
Arun Shankar <145361756+arun-shankar-s@users.noreply.github.com>
2024-10-13 21:11:37 +0530

    Create MainActivity.java

---
+++ Login Form/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.loginform;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText Username,Password;
+    Button Btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        Username=(EditText)findViewById(R.id.username);
+        Password=(EditText)findViewById(R.id.password);
+        Btn=(Button)findViewById(R.id.button);
+        Btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"Successful!!",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
